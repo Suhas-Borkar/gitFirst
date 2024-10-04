52a94f740557d3fd4bd47b5d211cbffc6f4941ac
Suhas-Borkar <123814349+Suhas-Borkar@users.noreply.github.com>
2024-10-04 22:26:32 +0530

    Add new boostartap css and js also add this files in angular .json file also change code form app.html file and add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component:FirstComponent} 
+];
